3433bfe50ca0379b7b9c5f3c86f2ae5abe2a0197
Chris Hurst <chris.hurst@stratusgrid.com>
2020-09-01 19:35:13 -0400

    added a description on output

--- -outputs.tf
+++ -outputs.tf
@@ -1,4 +1,5 @@
 output "local_tunnel_ips" {
+  description = "A map of the tunnel interface => IPs to dynamically add to OSPF, FW rules, etc."
   value = {
     for interface in fortios_system_interface.this:
         interface.name => interface.ip
