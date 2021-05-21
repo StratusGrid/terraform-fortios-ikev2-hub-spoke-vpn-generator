2e760ecb234ec9207a56542cd83c94552146deb1
Dick Childress <richard.childress@stratusgrid.com>
2021-05-21 17:12:58 -0400

    Output logic test.

--- -outputs.tf
+++ -outputs.tf
@@ -1,11 +1,8 @@
 output "local_tunnel_ips" {
 //TODO: add logic to output tunnel IPs if no system interfaces are created by terraform.
   description = "A map of the tunnel interface => IPs to dynamically add to OSPF, FW rules, etc."
-  value =
-    var.create_system_interfaces == true ?
-      for interface in fortios_system_interface.this :
-      interface.name => interface.ip
-    :
-      for interface in data.fortios_system_interface.this :
-      interface.name => interface.ip
+  value = {
+    for interface in merge(fortios_system_interface.this, data.fortios_system_interface.this) :
+    interface.name => interface.ip
+  }
 }
